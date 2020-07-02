8f9427907b94faf89c173ca0b23b5a34be954bef
Kalna Ranasinghe <kalnar328@gmail.com>
2020-07-02 15:05:21 +0530

    Camera function to capture photos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // url pointing to a storage location on device
+  photo: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  // this function is invoked by the button click
+  async capturePhoto(){
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl))
+
+  }
+
+
+
 
 }
